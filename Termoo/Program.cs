f6de560203ab3790fc491606bf71e4943ec8bfda
Anastácio <gl0inn@hotmail.com>
2022-09-25 11:10:02 -0300

    Changed logic in counting plays

--- Termoo/Program.cs
+++ Termoo/Program.cs
@@ -2,23 +2,15 @@
 
 System.Console.WriteLine("Welcome to Termoo Raro Console Program!!!");
 Termoo game = new Termoo();
-for (int i = 1; i <= 5; i++)
+do
 {
     System.Console.WriteLine();
-    System.Console.Write($"Write your #{i} word (5 letters): ");
+    System.Console.Write($"Write your #{game.Count + 1} word (5 letters): ");
     string word = Console.ReadLine();
-    System.Console.WriteLine();
     List<string> responses = game.Play(word);
+    System.Console.WriteLine();
     foreach (string response in responses)
     {
         System.Console.WriteLine(response);
     }
-    if (responses.Contains("Invalid Word"))
-    {
-        i--;
-    }
-    if (responses.Contains("You won!"))
-    {
-        break;
-    }
-}
+} while (game.Count <= 4);
